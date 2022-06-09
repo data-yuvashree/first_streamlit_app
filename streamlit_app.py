2906ae182950b0c8651c20c04f8ad6ce8378a909
Yuvashree P <107168812+data-yuvashree@users.noreply.github.com>
2022-06-09 11:21:21 +0530

    Update streamlit_app.py
    Added custom smoothie option in menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header("Menu For the Day")
 streamlit.text("Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale and Spinach Smoothie")
 streamlit.text("Hard Boiled Egg")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
