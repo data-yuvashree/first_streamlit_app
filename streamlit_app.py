53292ad3ec77c3ba681e8452375b52e43542b5ea
Yuvashree P <107168812+data-yuvashree@users.noreply.github.com>
2022-06-09 11:19:05 +0530

    Update streamlit_app.py
    Added menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
+
+stremlit.header("Menu For the Day")
+stremlit.text("Omega 3 and Blueberry Oatmeal")
+stremlit.text("Kale and Spinach Smoothie")
+stremlit.text("Hard Boiled Egg")
